75e5efc2089f430f10ad3f98cccc0b0ec6341a09
NAYANCSE27 <50147950+NAYANCSE27@users.noreply.github.com>
2022-07-26 23:16:30 +0600

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc141_a/7517443.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   string s;
+   cin>>s;
+   if (s[0]=='S')
+    cout<<"Cloudy"<<endl;
+   else if (s[0]=='R')
+    cout<<"Sunny"<<endl;
+   else
+    cout<<"Rainy"<<endl;
+
+    return 0;
+}
