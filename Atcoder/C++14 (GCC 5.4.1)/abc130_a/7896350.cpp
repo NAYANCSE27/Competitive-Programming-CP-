c93194b1a26bff499b7db516b5ff39bdf63d2148
NAYANCSE27 <50147950+NAYANCSE27@users.noreply.github.com>
2022-07-26 23:16:22 +0600

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc130_a/7896350.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,x;
+    cin>>x>>a;
+    if (x<a)
+        cout<<"0\n";
+    else
+        cout<<"10\n";
+
+    return 0;
+}
