57bb3fb80d71ad2aebb5efe2839872c51e42801f
NAYANCSE27 <50147950+NAYANCSE27@users.noreply.github.com>
2022-07-26 23:16:19 +0600

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc138_a/7897289.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    string s;
+    cin>>a>>s;
+    if (a<3200)
+        cout<<"red\n";
+    else
+        cout<<s<<endl;
+
+    return 0;
+}
