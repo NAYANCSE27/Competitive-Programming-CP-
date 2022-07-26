614fd6360ce761c0998058dd917df74968587781
NAYANCSE27 <50147950+NAYANCSE27@users.noreply.github.com>
2022-07-26 23:17:36 +0600

    Committing files

---
+++ CodeChef/C++17/FIRSTANDLAST/66192479.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define     fileIO      freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define     fast        ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define     vin(va,n)   for(int i=0; i<n; i++){int x;cin>>x;va.PB(x);}
+#define     lli         long long int
+#define     PB          push_back
+#define     MP          make_pair
+#define     endl        '\n'
+
+
+const int N=2e5+6;
+const int P1=1000000007;
+
+
+void testcases() {
+    lli n,mx;
+    vector<int>va;
+    cin>>n;
+    vin(va,n);
+    mx=va[0]+va[n-1];
+    for(int i=0; i<n-1; i++) {
+        lli mid=va[i]+va[i+1];
+        mx=max(mid,mx);
+    }
+    cout<<mx<<endl;
+}
+
+
+int main() {
+
+    fast;
+    int tc=1;
+    cin>>tc;
+    while(tc--)
+        testcases();
+
+    return 0;
+
+}
+
+
+/**
+
+
+Bitmasks/Prefix Sum/Binary Exponentiation/DFS/BFS
+/Binary Search/Recursion/GCD/LCM/Divisors/Knapsack/LCS
+
+
+*/
+
