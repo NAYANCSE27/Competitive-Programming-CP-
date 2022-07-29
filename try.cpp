4b4953e2de77504cc032a2c66efbb4fc2ebe6863
NAYAN_CSE27 <mehadihasannayan1998@gmail.com>
2022-07-30 00:42:04 +0600

    Air Conditioner done

--- try.cpp
+++ try.cpp
@@ -14,61 +14,35 @@ using namespace std;
 const int N=2e5+6;
 const int P1=1000000007;
 
-bool issubsequence(string& s1, string& s2)
-{
-    int n = s1.length(), m = s2.length();
-    int i = 0, j = 0;
-    while (i < n && j < m) {
-        if (s1[i] == s2[j])
-            i++;
-        j++;
-    }
-    return i == n;
-}
-
 
 void testcases() {
-    string s,t;
-    int a[33],b[33];
-    memset(a,0,sizeof(a));
-    memset(b,0,sizeof(b));
-    cin>>s>>t;
+    int n,m,t[111],l[111],r[111];
+    cin>>n>>m;
+    for(int i=0; i<n; i++)  cin>>t[i]>>l[i]>>r[i];
 
-    for(int i=0; i<s.size(); i++)   a[s[i]-'a'+1]++;
-    for(int i=0; i<t.size(); i++)   b[t[i]-'a'+1]++;
-    
-    if(s.size()==t.size()) {
-        bool f=true;
-        for(int i=1; i<=26; i++) {
-            if(a[i]!=b[i]) {
-                f=false;
-                break;
-            }
+    int prev=0,mx=m,mn=m;
+    bool flag=true;
+    for(int i=0; i<n; i++) {
+        mx+=t[i]-prev;
+        mn-=t[i]-prev;
+        if(mx<l[i]||mn>r[i]) {
+            flag=false;
+            break;
         }
-
-        if(f)       cout<<"array"<<endl;
-        else        cout<<"need tree"<<endl;
-    }else {
-
-        bool f=true,ff=true;
-        for(int i=1; i<=26; i++) {
-            if(a[i]>b[i])       f=false;
-            else if(b[i]>a[i])  ff=false;
-        }
-
-        bool flag=issubsequence(t,s);
-        if(flag)        cout<<"automaton"<<endl;
-        else {
-            if(ff==false)       cout<<"need tree"<<endl;
-            else                cout<<"both"<<endl;
-        }
+        mx=min(mx,r[i]);
+        mn=max(mn,l[i]);
+        prev=t[i];
     }
+
+    if(flag)    cout<<"YES"<<endl;
+    else        cout<<"NO"<<endl;
 }
 
 int main() {
 
     fast;
     int tc=1;
+    cin>>tc;
     while(tc--)
         testcases();
 
