4af68d43bbc8f4fce0c3cf53e1ad905e39755344
NAYAN_CSE27 <mehadihasannayan1998@gmail.com>
2022-08-05 18:44:41 +0600

    abc 77 a done

---
+++ Atcoder/abc77/a.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define     fileIO      freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define     fast        ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define     vin(va,n)   for(int i=0; i<n; i++){int x;cin>>x;va.PB(x);}
+#define     lli         long long int
+#define     PB          push_back
+#define     MP          make_pair
+#define     endl        '\n'
+
+
+const int N=2e5+6;
+const int P1=1000000007;
+
+
+void testcases() {
+    string s,ss;
+    cin>>s>>ss;
+    if(s[1]==ss[1]) {
+        if(s[0]==ss[2]&&s[2]==ss[0])    cout<<"YES"<<endl;
+        else                            cout<<"NO"<<endl;
+    }else       cout<<"NO"<<endl;
+}
+
+int main() {
+
+    fast;
+    int tc=1;
+    //cin>>tc;
+    while(tc--)
+        testcases();
+
+    return 0;
+
+}
+
+
+/**
+
+
+Bitmasks/Prefix Sum/Binary Exponentiation/DFS/BFS
+/Binary Search/Recursion/GCD/LCM/Divisors/Knapsack/LCS
+
+
+*/
