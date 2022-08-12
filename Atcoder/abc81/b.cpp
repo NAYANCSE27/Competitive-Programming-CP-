f0db85a4e2ccb28234fa1527e476e9878cae8b43
NAYAN_CSE27 <mehadihasannayan1998@gmail.com>
2022-08-12 21:54:38 +0600

    abc81 b done

---
+++ Atcoder/abc81/b.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define     fileIO      freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define     fast        ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define     vin(va,n)   for(int i=0; i<n; i++){int x;cin>>x;va.PB(x);}
+#define     lli         long long int
+#define     PB          push_back
+#define     MP          make_pair
+#define     endl        '\n'
+
+
+const int N=2e5+6;
+const int P1=1000000007;
+
+
+void testcases() {
+    int n;
+    vector<int>va,vb;
+    cin>>n;
+    vin(va,n);
+
+    for(int i=0; i<n; i++) {
+        int ctn=0;
+        while (va[i]%2==0){
+            ctn++;
+            va[i]/=2;
+        }
+        vb.PB(ctn);
+    }
+    sort(vb.begin(), vb.end());
+    cout<<vb[0]<<endl;
+}
+
+int main() {
+
+    fast;
+    int tc=1;
+    //cin>>tc;
+    while(tc--)
+        testcases();
+
+    return 0;
+
+}
+
+
+/**
+
+
+Bitmasks/Prefix Sum/Binary Exponentiation/DFS/BFS
+/Binary Search/Recursion/GCD/LCM/Divisors/Knapsack/LCS
+
+
+
+*/
